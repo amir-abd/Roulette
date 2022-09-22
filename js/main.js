a22ae5dd24604367cf6bb9750ae8fc8bad5d69c2
usernameamirabdgit config --global user.name usernameamirabd <amirabdollahi24@gmail.com>
2022-09-21 17:11:26 -0700

    added buttons and some functions

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ let total = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
 
 let old_id = 100
 
-
+/*----- app's state (variables) -----*/
+let winner, board
 
 /*----- cached element references -----*/
 const resetBtnEl = document.getElementById('reset')
+const exitBtnEl = document.getElementById('exit')
+const spinBtnEl = document.getElementById('spin')
 const messageDisplayEl = document.querySelector('h2')
 
+
+/*----- event listeners -----*/
+// resetBtnEl.addEventListener('click', handleResetClick)
+spinBtnEl.addEventListener('click', handleSpinClick)
+// boardEl.addEventListener('click', handleBoardClick)
+// bet.add
 
 function myFunction(event) { 
     alert(event.target.id);
@@ -26,8 +35,28 @@ function buttonPressed(evt) {
     old_id=evt.target.id;
 }
 
-function 
-//function init() {
-//}
+// function init() {
+//     board = [null, null, null, null, null, null, null, null, null]
+//     bank = 50
+// }
 
-//init();
+
+// function checkWin() {
+//     if (bet.id === winNum) {
+//         messageDisplayEl.innerText = 'Win'
+//     }else{
+//         messageDisplayEl.innerText = 'No Win'
+//     }
+
+
+// function handleResetClick() {
+//     init()
+//     }
+
+function handleSpinClick() {
+    let winNum = Math.floor(Math.random() * 36) 
+    return }
+
+function bet() {
+    let bet = 0
+}
